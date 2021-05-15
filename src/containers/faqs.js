654f9db893699dfbcbffa65e6e80227419f7a2c2
dionce <dionisenja@gmail.com>
2021-05-15 11:18:36 +0200

    Built OptForm

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import faqsData from "../fixtures/faqs.json";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 
 export function FaqsContainer() {
   return (
@@ -12,6 +12,14 @@ export function FaqsContainer() {
         </Accordion.Item>
       ))}
       <Accordion.Item></Accordion.Item>
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to Watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
