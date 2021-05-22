41efa1ef9a009f463468ad4eda082ca1b0e87231
dionce <dionisenja@gmail.com>
2021-05-22 10:56:46 +0200

    Added custom hooks for get user and data fetching

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,4 +1,8 @@
+import { useContent } from "../hooks";
+
 const Browse = () => {
+  const { series } = useContent("series");
+  console.log(series);
   return <div>Hello from the browse</div>;
 };
 
