a287177c84a8b365cb91ac2ec9f52115db55195b
dionce <dionisenja@gmail.com>
2021-05-27 11:46:38 +0200

    Card list, Feature component and styling

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -6,7 +6,7 @@ const Browse = () => {
   const { series } = useContent("series");
   const { films } = useContent("films");
 
-  const slides = selectionFilter(series, films);
+  const slides = selectionFilter({ series, films });
 
   return <BrowseContainer slides={slides} />;
 };
